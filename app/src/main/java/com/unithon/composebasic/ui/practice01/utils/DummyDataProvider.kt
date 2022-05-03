706f8df54dae6e0c5f66f5e904729ca8c05eaea8
tmdgh1592 <tmdgh1592@naver.com>
2022-05-04 00:20:25 +0900

    Create a user listview using LazyColumn

---
+++ app/src/main/java/com/unithon/composebasic/ui/practice01/utils/DummyDataProvider.kt
@@ -0,0 +1,13 @@
+package com.unithon.composebasic.ui.practice01.utils
+
+data class RandomUser(
+    val name : String = "최승호",
+    val description: String = "오늘도 빡코딩 하고 계신가요?",
+    val profileImage: String = "https://randomuser.me/api/portraits/women/57.jpg"
+)
+
+object DummyDataProvider {
+    val userList = List<RandomUser>(200) {
+        RandomUser()
+    }
+}
