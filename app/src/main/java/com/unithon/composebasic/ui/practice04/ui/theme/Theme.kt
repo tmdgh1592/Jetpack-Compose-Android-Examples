30e0d793486342edef73475cf97acdd92a622f7b
tmdgh1592 <tmdgh1592@naver.com>
2022-05-04 23:34:33 +0900

    Practice BoxWithConstraints

--- app/src/main/java/com/unithon/composebasic/ui/practice04/ui/theme/Theme.kt
+++ app/src/main/java/com/unithon/composebasic/ui/practice04/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.unithon.composebasic.ui.practice03.ui.theme
+package com.unithon.composebasic.ui.practice04.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = com.unithon.composebasic.ui.practice04.ui.theme.Purple200,
-    primaryVariant = com.unithon.composebasic.ui.practice04.ui.theme.Purple700,
-    secondary = com.unithon.composebasic.ui.practice04.ui.theme.Teal200
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = com.unithon.composebasic.ui.practice04.ui.theme.Purple500,
-    primaryVariant = com.unithon.composebasic.ui.practice04.ui.theme.Purple700,
-    secondary = com.unithon.composebasic.ui.practice04.ui.theme.Teal200
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
 
     /* Other default colors to override
     background = Color.White,
